bc1ca223df7b5269cdec26ba90b5af9e2f851fdd
Andres <aguirregzz97@gmail.com>
2020-11-18 16:36:15 -0600

    Initial Setup Project

---
+++ src/Components/Home.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+type Props = { }
+
+type State = { }
+
+export class Home extends React.Component<Props, State> {
+    
+    constructor(props: Props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <>
+                <h1>HELLO</h1>
+            </>
+        )
+    }
+
+
+}
